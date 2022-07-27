597b040b21e1b36bfe19af94e1477aa59695092f
Sander van Maurik <s.vanmaurik@jasbit.nl>
2022-07-27 23:17:29 +0200

    getting random quote on pageload

--- quote/src/components/main/main.tsx
+++ quote/src/components/main/main.tsx
@@ -4,19 +4,17 @@ import QuoteBanner from '../quote-banner/quoteBanner';
 import QuoteVotingBox from '../quote-voting-box/quoteVotingBox';
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
 import "../main/main.scss"
+import { doRequest, vote } from '../../helpers/utils';
 
 const Main: React.FC = () => {
-  const [currentQuote, setCurrentQuote] = useState<Quote>(new Quote());
-  const [allQuotes, setAllQuotes] = useState<Quote[]>([]);
+
   const [visitorId, setVisitorId] = useState<string>("");
-  const baseURL: string = "http://localhost:5000";
+  const [currentQuote, setCurrentQuote] = useState<Quote>({});
 
   useEffect(() => {
-    // get data
-    fetch(`${baseURL}/getquotes`)
-      .then(response => response.json())
-      .then((x: Quote[]) => {
-        setQuotes(x);
+    doRequest('/quotes/random', 'GET')
+      .then((rndQuote: Quote) => {
+        setCurrentQuote(quote => quote = rndQuote);
       });
   }, []);
 
@@ -29,47 +27,18 @@ const Main: React.FC = () => {
       });
   }, []);
 
-  useEffect(() => {
-    let randomQuote = getRandomQuote(allQuotes as Quote[]);
-    setCurrentQuote(randomQuote as Quote);
-
-  }, [allQuotes]);
-
-
   const handleVote = (ratingId: string) => {
-    if (currentQuote.id) {
-      vote(currentQuote.id, visitorId, ratingId).then(resp => {
-        setCurrentQuote(resp as Quote);
-      });
+    if (currentQuote?.id) {
+      vote(currentQuote.id, ratingId, visitorId).then(resp => {
+        setCurrentQuote(prevQuote => resp as Quote);
+      })
     }
-  }
-
-  const getQuoteById = (quoteId: any) => {
-    return fetch(`${baseURL}/quotes/${quoteId}`)
-      .then(response => response.json())
-  }
-
-  const getRandomQuote = (quotes: Quote[]) => {
-    return (quotes) ? quotes[Math.floor(Math.random() * (quotes.length - 1))] : null;
-  }
-
-  const setQuotes = (quotes: Quote[]): void => {
-    setAllQuotes(quotes);
-  }
-
-  const vote = (quoteId: string, userId: string, voteOptionId: string): Promise<Response> => {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ userId: userId, vote: voteOptionId })
-    };
-    return fetch(`${baseURL}/quotes/${quoteId}`, requestOptions).then(response => response.json());
   }
 
   return (
     <div className='container'>
       <QuoteBanner quote={currentQuote?.quote!} />
-      <QuoteVotingBox onClick={handleVote} rating={currentQuote?.rating} visitorId={visitorId}/>
+      <QuoteVotingBox onClick={handleVote} rating={currentQuote?.rating} visitorId={visitorId} />
     </div>
   )
 
