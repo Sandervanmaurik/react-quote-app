f40d5b113f97a3080f6b8524338a199675e645b8
Sander van Maurik <s.vanmaurik@jasbit.nl>
2022-08-04 11:46:42 +0200

    Shortened request function

--- quote/src/utils/utils.ts
+++ quote/src/utils/utils.ts
@@ -1,21 +1,14 @@
-const someCommonValues = ['common', 'values'];
 const baseURL: string = "http://localhost:5000";
 
 export const doRequest = (url: any, method: any, body?: any): Promise<any> => {
-   let requestOptions;
-   if(body){
-      requestOptions = {
-         headers: { 'Content-Type': 'application/json' },
-         method: method,
-         body: JSON.stringify(body)
-      }
-   }
-   else{
-      requestOptions = {
-         headers: { 'Content-Type': 'application/json' },
-         method: method
-      }
-   }
+   let requestOptions: RequestInit;
+   requestOptions = {
+      headers: { 'Content-Type': 'application/json' },
+      method: method,
+      body: JSON.stringify(body)
+   };
+
+   body = (body) ? JSON.stringify(body) : null;
    return fetch(`${baseURL}${url}`, requestOptions)
       .then(response => response.json());
 };
