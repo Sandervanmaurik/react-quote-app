e33d26343f43f864db0ca90c5b5f7587d8c77ac1
Sander van Maurik <s.vanmaurik@jasbit.nl>
2022-08-04 16:20:58 +0200

    Added simple-quotes endpoint

--- quote-service/index.ts
+++ quote-service/index.ts
@@ -14,6 +14,8 @@ let allQuotes: Quote[] = [];
 
 app.use(express.json());
 
+type simpleQuote = { id: string, hasVoted: boolean };
+
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
     fetch('http://quotes.stormconsultancy.co.uk/quotes.json')
@@ -24,7 +26,7 @@ app.listen(port, () => {
                     id: x["id"],
                     quote: x["quote"],
                     author: x["author"],
-                    rating: [{ id: "AAA", name: "Boring", voters: ['2c951a4a54a5b4296d96ddd16b592bc3'], color:"#F1F2F6", icon:("<ImSleepy color="#c9ccd6" fontSize="30px" />")}, { id: "BBB", name: "I don't get it", voters: [], color:"#EFEBFF" }, { id: "CCC", name: "Funny", voters: [], color:"#D6EBE5" }, { id: "DDD", name: "Inspiring", voters: [], color:"#FEF3D7" }]
+                    rating: [{ id: "AAA", name: "Boring", voters: ['2c951a4a54a5b4296d96ddd16b592bc3'], color: "#F1F2F6", icon: "sleepy" }, { id: "BBB", name: "I don't get it", voters: [], color: "#EFEBFF", icon: "questionMark" }, { id: "CCC", name: "Funny", voters: [], color: "#D6EBE5", icon: "laugh" }, { id: "DDD", name: "Inspiring", voters: [], color: "#FEF3D7", icon: "idea" }]
                 }
                 allQuotes.push(quote);
             });
@@ -32,13 +34,25 @@ app.listen(port, () => {
 });
 
 app.get('/quotes/random', (req: any, res: any) => {
-    let randomQuote: Quote =  (allQuotes) ? allQuotes[Math.floor(Math.random() * (allQuotes.length - 1))] : null;
+    let randomQuote: Quote = (allQuotes) ? allQuotes[Math.floor(Math.random() * (allQuotes.length - 1))] : null;
     res.send(JSON.stringify(randomQuote));
 });
 
 
 app.get('/quotes', (req: any, res: any) => {
     res.send(allQuotes);
+});
+
+app.get('/simple-quotes', (req: any, res: any) => {
+    let userId = req.query.userId;
+    let simpleQuotes: simpleQuote[] = [];
+    allQuotes.forEach(x => {
+        simpleQuotes.push({
+            id: x.id,
+            hasVoted: userHasVoted(userId, x.id)
+        });
+    });
+    res.send(JSON.stringify(simpleQuotes));
 });
 
 app.get('/quotes/:quoteId', (req: any, res: any) => {
@@ -49,7 +63,6 @@ app.post('/quotes/:quoteId', (req: any, res: any) => {
     let userId = req.body.userId;
     let voteOptionId = req.body.vote;
     let quoteId = req.params.quoteId;
-
     let quote = allQuotes.find(x => x.id == quoteId);
     if (quote) {
         for (let rating of quote.rating) {
@@ -64,5 +77,17 @@ app.get('/quotes/:quoteId', (req: any, res: any) => {
     let quoteId = req.params.quoteId;
     res.send(allQuotes.find(x => x.id === quoteId));
 });
+
+
+function userHasVoted(userId: string, quoteId: string){
+    let quote = allQuotes.find(quote => quote.id === quoteId);
+    let found = false;
+    quote.rating.forEach(rating => {
+        if(rating.voters.find(voter => voter == userId)){
+            found = true;
+        }
+    })
+    return found;
+}
 
 
