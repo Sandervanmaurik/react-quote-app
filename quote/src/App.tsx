105ef18de5c8c752c3b0224e1e8fd7936bbcd5ce
Sander van Maurik <s.vanmaurik@jasbit.nl>
2022-08-07 14:55:01 +0200

    Moved browserrouter

--- quote/src/App.tsx
+++ quote/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
 import Home from './pages/home/home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import NavigationBar from './components/navigationBar/navigationBar';
 import { Quote } from './models/quote';
 import { doRequest } from './utils/utils';
@@ -9,11 +9,11 @@ import { doRequest } from './utils/utils';
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       < Routes >
         <Route path="/" element={<Home />}></Route>
       </Routes >
-    </>
+    </BrowserRouter>
   );
 
 }
